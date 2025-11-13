f0a11da2656adc2fb3010fea99ef5bcbda029bf0
Burben-Kapula <burbenburbenov@gmail.com>
2025-11-13 13:10:58 +0200

    3131

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors());
-// ...тут інші маршрути та middleware...
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Якщо приходить будь-який інший запит (наприклад, /route), повертаємо index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
