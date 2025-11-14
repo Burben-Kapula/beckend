9176aa801e817dbd5b3671225aa938c0ff9371e9
Burben-Kapula <burbenburbenov@gmail.com>
2025-11-14 08:43:12 +0200

    4141

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
-const app = express();
-
-app.use(cors());
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all: працює на express 5.x
-app.get('/:any*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
-
-const PORT = process.env.PORT || 3001;
+app.use(express.static('dist'))
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
