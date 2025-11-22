4a0f5b5121992f4c384c321df0136359d3bcb8b5
Burben <burbenburbenov@gmail.com>
2025-11-22 10:10:47 +0200

    add minlength

--- models/person.js
+++ models/person.js
@@ -1,7 +1,17 @@
 // models/person.js
 const mongoose = require('mongoose');
+
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,   // мінімум 3 символи
+    required: true  // обов’язкове поле
+  },
+  number: {
+    type: String,
+    minlength: 8,   // мінімум 8 символів (або свою вимогу)
+    required: true  // обов’язкове поле
+  }
 });
+
 module.exports = mongoose.model('Person', personSchema);
