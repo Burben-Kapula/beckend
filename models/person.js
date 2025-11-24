5410b5112180d749c969519439f3d5b6e77a2c1c
Burben <burbenburbenov@gmail.com>
2025-11-24 12:20:23 +0200

    4

--- models/person.js
+++ models/person.js
@@ -1,24 +1,20 @@
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 
-const phoneRegex = /^(\d{2,3})-\d{5,}$/;
-
-const personSchema = new mongoose.Schema({
-  name: {
+const noteSchema = new mongoose.Schema({
+  content: {
     type: String,
-    minlength: 3,
-    required: true
-  },
-  number: {
-    type: String,
-    minlength: 8, // загальна мінімальна довжина
     required: true,
-    validate: {
-      validator: function(v) {
-        return phoneRegex.test(v);
-      },
-      message: props => `${props.value} is not a valid phone number!`
-    }
-  }
-});
+    minlength: 5
+  },
+  important: Boolean,
+})
 
-module.exports = mongoose.model('Person', personSchema);
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
