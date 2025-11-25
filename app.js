df03effdfdab54be8a043cd0e0c8427ebd6d4cbc
Burben <burbenburbenov@gmail.com>
2025-11-25 13:30:13 +0200

    13243546789809

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 const middleware = require('./utils/middleware')
-const notesRouter = require('./controllers/notes')
+const personsRouter = require('./controllers/persons')
+
+app.use('/api/persons', personsRouter)
 
 const app = express()
 
@@ -22,7 +24,6 @@ app.use(express.static('dist'))
 app.use(express.json())
 app.use(middleware.requestLogger)
 
-app.use('/api/person', notesRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
