2df61796fb6e7a499a0f894abe9e7a2a88e3a5a6
Burben <burbenburbenov@gmail.com>
2025-11-25 13:22:50 +0200

    435e46r7t8yujip

--- test/note_api.test.js
+++ test/note_api.test.js
@@ -3,58 +3,58 @@ const assert = require('node:assert')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-const Note = require('../models/person')
+const Person = require('../models/person')
 
 const api = supertest(app)
 
-const initialNotes = [
+const initialPersons = [
   { content: 'HTML is easy', important: false },
   { content: 'Browser can execute only JavaScript', important: true },
 ]
 
 // Очищення й підготовка колекції перед тестами
 beforeEach(async () => {
-  await Note.deleteMany({})
-  for (const note of initialNotes) {
-    await new Note(note).save()
+  await Person.deleteMany({})
+  for (const person of initialPersons) {
+    await new Person(person).save()
   }
 })
 
-test('notes are returned as json', async () => {
+test('persons are returned as json', async () => {
   await api
-    .get('/api/notes')
+    .get('/api/persons')
     .expect(200)
     .expect('Content-Type', /application\/json/)
-}) // присутній у курсі [web:64][web:75]
+})
 
-test('all notes are returned', async () => {
-  const response = await api.get('/api/notes')
-  assert.strictEqual(response.body.length, initialNotes.length)
-}) // присутній у курсі [web:64][web:75]
+test('all persons are returned', async () => {
+  const response = await api.get('/api/persons')
+  assert.strictEqual(response.body.length, initialPersons.length)
+})
 
-test('a specific note is within the returned notes', async () => {
-  const response = await api.get('/api/notes')
+test('a specific person is within the returned persons', async () => {
+  const response = await api.get('/api/persons')
   const contents = response.body.map(e => e.content)
   assert.strictEqual(contents.includes('HTML is easy'), true)
-}) // присутній у курсі [web:64][web:75]
+})
 
-test('a valid note can be added', async () => {
-  const newNote = {
+test('a valid person can be added', async () => {
+  const newPerson = {
     content: 'async/await simplifies making async calls',
     important: true,
   }
   await api
-    .post('/api/notes')
-    .send(newNote)
+    .post('/api/persons')
+    .send(newPerson)
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/notes')
+  const response = await api.get('/api/persons')
   const contents = response.body.map(r => r.content)
-  assert.strictEqual(response.body.length, initialNotes.length + 1)
+  assert.strictEqual(response.body.length, initialPersons.length + 1)
   assert(contents.includes('async/await simplifies making async calls'))
-}) // присутній у курсі [web:64][web:75]
+})
 
 after(async () => {
   await mongoose.connection.close()
-}) // присутній у курсі [web:64][web:75]
+})
